b81723a6e76cf7e99c090c66a6da1ba7993a9833
Ian Horsman <ianphorsman@gmail.com>
2016-06-28 14:47:42 -0400

    Helper methods to identify and hash user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,31 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def authenticate_or_create_user
+    ip_addr = request.remote_ip
+    device = request.user_agent
+
+    hashable_user_string = "#{ip_addr}_#{device}"
+
+    hashed_user = Digest::SHA256.new.digest(hashable_user_string)
+
+    user = User.find_by_hashed_identity(hashed_user)
+    if user.nil?
+      User.create({hashed_identity: hashed_user })
+    end
+    user
+  end
+
+  def user_participated?
+    user = authenticate_or_create_user!
+    if user.polls.find_by_id(params[:poll_id])
+      true
+    else
+      false
+    end
+  end
+
 end
